bb8eb5b616de8f7480ce7d949caf562773988dc5
patchigollagowtham-commits <patchigollagowtham@gmail.com>
2026-07-22 22:09:57 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,22 @@
+
+#first_name="sai"
+#last_name="Gowtham"
+#full_name=first_name+last_name
+#print("Hello"+full_name)
+
+#age=19
+#print("Your age is "+str(age))
+#height=250.5
+#print(height)
+
+#a=10
+#b=6
+#print(a+b)
+#print(a-b)
+#print(a*b)
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
